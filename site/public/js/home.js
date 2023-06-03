bdf744712678794a66a28ac7b446134b5df4103b
Kauan <kauaanmatheus@gmail.com>
2023-06-03 01:19:46 -0300

    Conclusão do projeto

--- site/public/js/home.js
+++ site/public/js/home.js
@@ -6,12 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("a-home").style.opacity = "1";
       setTimeout(() => {
         document.getElementById("a-personagens").style.opacity = "1";
-        setTimeout(() => {
-          document.getElementById("a-desenvolvedor").style.opacity = "1";
           setTimeout(() => {
             document.getElementById("a-quiz").style.opacity = "1";
           }, 200);
-        }, 200);
       }, 200);
     }, 1000);
   }, 1000);
